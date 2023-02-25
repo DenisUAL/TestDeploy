35a63798f93fda1882394f2e42888744dc4962c2
Denys <denys@pixelmii.com>
2023-02-25 13:22:37 +0200

    aaaaaa

--- src/Pages/MoviePage/MoviePage.js
+++ src/Pages/MoviePage/MoviePage.js
@@ -1,29 +1,28 @@
-import {useEffect} from "react";
-import {useParams} from "react-router-dom";
-import {useDispatch, useSelector} from "react-redux";
-import {moviesActions} from "../../redux";
+import { useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { moviesActions } from "../../redux";
 
-import {Header, MovieInfo} from "../../components";
+import { Header, MovieInfo } from "../../components";
 
 const MoviePage = () => {
+  const { id } = useParams();
 
-    const {id} = useParams();
+  let { currentInfo } = useSelector((state) => state.info);
 
-    let {currentInfo} = useSelector(state => state.info);
+  let dispatch = useDispatch();
 
-    let dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(moviesActions.getMovieInfo({ id }));
+  }, [dispatch, id]);
 
-    useEffect(() => {
-        dispatch(moviesActions.getMovieInfo({id}))
-    }, [dispatch, id]);
-
-
-    return (
-        <div>
-            <Header/>
-            {currentInfo && <MovieInfo info={currentInfo}/>}
-        </div>
-    );
+  return (
+    <div>
+      aaaaaaaa
+      <Header />
+      {currentInfo && <MovieInfo info={currentInfo} />}
+    </div>
+  );
 };
 
-export {MoviePage};
+export { MoviePage };
